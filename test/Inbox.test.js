ce59ac39d8b3bc5c4056393eed6d8425d4cb4f5d
Richard Pryce <richard.pryce@engineerbetter.com>
2021-12-13 23:34:20 +0000

    Verify set message

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -25,4 +25,9 @@ describe('Inbox', () => {
         const message = await inbox.methods.message().call();
         assert.equal(message, INITIAL_STRING)
     })
+    it('can change the message', async ()=>{
+        await inbox.methods.setMessage('test').send({ from: accounts[0] })
+        const message = await inbox.methods.message().call();
+        assert.equal(message, "test")
+    })
 });
