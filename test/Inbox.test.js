0678e4b97b65b050c56b5518e26ed46a94f625aa
Richard Pryce <richard.pryce@engineerbetter.com>
2021-12-13 23:28:25 +0000

    Verify initial message

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,21 +4,25 @@ const { beforeEach } = require('mocha');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 const { interface, bytecode } = require('../compile');
-console.log(bytecode)
 
-let accounts
+let accounts;
 let inbox;
+const INITIAL_STRING = "Hi there!"
 beforeEach(async() => {
     // Get a list of all accounts
     accounts = await web3.eth.getAccounts()
     // Use one of those accounts to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!']})
+    .deploy({ data: bytecode, arguments: [INITIAL_STRING]})
     .send({ from: accounts[0], gas: '1000000' })
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(inbox);
+        assert.ok(inbox.options.address);
+    });
+    it('has a default message', async () =>{
+        const message = await inbox.methods.message().call();
+        assert.equal(message, INITIAL_STRING)
     })
-})
+});
